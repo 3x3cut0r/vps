24c71ff5fc073ae032f1617f97441288b25b95e0
3x3cut0r <executor55@gmx.de>
2021-02-25 21:11:17 +0100

    fail2ban.sh

--- basics/fail2ban.sh
+++ basics/fail2ban.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 apt install fail2ban -y
 if [ $(cat /etc/fail2ban/jail.d/defaults-debian.conf | grep sshd) = "[sshd]" ]; then
+    echo "remove /etc/fail2ban/jail.d/defaults-debian.conf"
     rm /etc/fail2ban/jail.d/defaults-debian.conf
 fi
+echo "create /etc/fail2ban/jail.d/sshd.conf"
 echo -e "[sshd]\nenabled = true" > /etc/fail2ban/jail.d/sshd.conf
+echo "systemctl restart fail2ban"
 systemctl restart fail2ban
+fail2ban-client status
