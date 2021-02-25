4bdc710264bc7bba87242dc213693a16aa423b33
3x3cut0r <executor55@gmx.de>
2021-02-25 21:44:05 +0100

    change_hostname.sh

--- basics/change_hostname.sh
+++ basics/change_hostname.sh
@@ -2,8 +2,8 @@
 if [ $1 ]; then
     echo "$1" > /etc/hostname
     read -p "Do you want to reboot now? (y/N): "
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm -- "$0"
         /sbin/reboot
     fi
 fi
