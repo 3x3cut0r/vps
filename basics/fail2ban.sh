b48763f59f9fec3370049a5dd7d6d99e4a932463
3x3cut0r <executor55@gmx.de>
2021-03-03 09:53:34 +0100

    fail2ban.sh

--- basics/fail2ban.sh
+++ basics/fail2ban.sh
@@ -5,7 +5,16 @@ if [ $(cat /etc/fail2ban/jail.d/defaults-debian.conf | grep sshd) = "[sshd]" ]; 
     rm /etc/fail2ban/jail.d/defaults-debian.conf
 fi
 echo "create /etc/fail2ban/jail.d/sshd.conf"
-echo -e "[sshd]\nenabled = true" > /etc/fail2ban/jail.d/sshd.conf
+echo "[sshd]" > /etc/fail2ban/jail.d/sshd.conf
+echo "enabled   = true" >> /etc/fail2ban/jail.d/sshd.conf
+echo "port      = ssh" >> /etc/fail2ban/jail.d/sshd.conf
+echo "filter    = sshd[mode=aggressive]" >> /etc/fail2ban/jail.d/sshd.conf
+echo "logpath   = /var/log/auth.log" >> /etc/fail2ban/jail.d/sshd.conf
+echo "banaction = iptables-ipset-proto6" >> /etc/fail2ban/jail.d/sshd.conf
+echo "maxretry  = 5" >> /etc/fail2ban/jail.d/sshd.conf
+echo "findtime  = 1h" >> /etc/fail2ban/jail.d/sshd.conf
+echo "bantime   = 2w" >> /etc/fail2ban/jail.d/sshd.conf
+echo "ignoreip  = 127.0.0.1/8" >> /etc/fail2ban/jail.d/sshd.conf
 echo "systemctl restart fail2ban"
 systemctl restart fail2ban
 fail2ban-client status
