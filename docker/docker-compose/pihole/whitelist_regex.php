27192827be27b9bfdac6eaee8e508aa5d93144cf
3x3cut0r <executor55@gmx.de>
2021-12-12 14:04:21 +0100

    pihole/whitelist_regex.php

--- docker/docker-compose/pihole/whitelist_regex.php
+++ docker/docker-compose/pihole/whitelist_regex.php
@@ -1,2 +1,18 @@
+# 3x3cut0r
 3x3cut0r.de$
 3x3cut0r.synology.me$
+
+# Reddit
+[a-z]\.thumbs\.redditmedia\.com
+(\.|^)redd\.it$
+(\.|^)reddit\.com$
+
+# Skype
+sa.symcb.com s{1..5}.symcb.com
+
+# Twitter
+(\.|^)twimg\.com$
+
+# Whatsapp
+^whatsapp-cdn-shv-[0-9]{2}-[a-z]{3}[0-9]\.fbcdn\.net$
+^((www|(w[0-9]\.)?web|media((-[a-z]{3}|\.[a-z]{4})[0-9]{1,2}-[0-9](\.|-)(cdn|fna))?)\.)?whatsapp\.(com|net)$
