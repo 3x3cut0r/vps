097adc2738aa7d88e7710e73d359304ef79d6ab6
3x3cut0r <executor55@gmx.de>
2021-12-12 14:04:13 +0100

    pihole/whitelist.php

--- docker/docker-compose/pihole/whitelist.php
+++ docker/docker-compose/pihole/whitelist.php
@@ -1,1 +1,379 @@
+##########################
+### Apple                #
+##########################
+
+# Apple Music
+itunes.apple.com
+s.mzstatic.com
+
+# Apple ID
+appleid.apple.com
+
+# iOS Weather
+gsp-ssl.ls.apple.com
+gsp-ssl.ls-apple.com.akadns.net
+
+##########################
+### Captive-portal test  #
+##########################
+
+# Android/Chrome
+connectivitycheck.android.com
+android.clients.google.com
+clients3.google.com
+connectivitycheck.gstatic.com
+
+# iOS/Apple
+captive.apple.com
+gsp1.apple.com
+www.apple.com
+www.appleiphonecell.com
+
+# Windows/Microsoft
+msftncsi.com
+www.msftncsi.com
+ipv6.msftncsi.com
+
+##########################
+### Google               #
+##########################
+
+# Google Chrome (updater)
+dl.google.com
+
+# Google Fonts
+gstaticadssl.l.google.com
+
+# Google Keep
+reminders-pa.googleapis.com firestore.googleapis.com
+
+# Google Mail
+googleapis.l.google.com
+
+# Google Maps and other Google Services
+clients2.google.com
+clients4.google.com
+
+# Google Play
+android.clients.google.com
+
+# Youtube App for iOS
+www.googleapis.com
+youtubei.googleapis.com
+oauthaccountmanager.googleapis.com
+
+# Youtube History
+s.youtube.com
+video-stats.l.google.com
+
+##########################
+### Meta                 #
+##########################
+
+# Facebook, Messenger
+upload.facebook.com
+creative.ak.fbcdn.net
+external-lhr0-1.xx.fbcdn.net
+external-lhr1-1.xx.fbcdn.net
+external-lhr10-1.xx.fbcdn.net
+external-lhr2-1.xx.fbcdn.net
+external-lhr3-1.xx.fbcdn.net
+external-lhr4-1.xx.fbcdn.net
+external-lhr5-1.xx.fbcdn.net
+external-lhr6-1.xx.fbcdn.net
+external-lhr7-1.xx.fbcdn.net
+external-lhr8-1.xx.fbcdn.net
+external-lhr9-1.xx.fbcdn.net
+fbcdn-creative-a.akamaihd.net
+scontent-lhr3-1.xx.fbcdn.net
+scontent.xx.fbcdn.net
+scontent.fgdl5-1.fna.fbcdn.net
+graph.facebook.com
+b-graph.facebook.com
+connect.facebook.com
+cdn.fbsbx.com
+api.facebook.com
+edge-mqtt.facebook.com
+mqtt.c10r.facebook.com
+portal.fb.com
+star.c10r.facebook.com
+star-mini.c10r.facebook.com
+b-api.facebook.com
+fb.me bigzipfiles.facebook.com
+l.facebook.com
+www.facebook.com
+scontent-atl3-1.xx.fbcdn.net
+static.xx.fbcdn.net
+edge-chat.messenger.com
+video.xx.fbcdn.net
+external-ort2-1.xx.fbcdn.net
+scontent-ort2-1.xx.fbcdn.net
+edge-chat.facebook.com
+scontent-mia3-1.xx.fbcdn.net
+web.facebook.com
+rupload.facebook.com
+
+##########################
+### Microsoft            #
+##########################
+
+# Bing Maps Platform
+dev.virtualearth.net
+ecn.dev.virtualearth.net
+t0.ssl.ak.dynamic.tiles.virtualearth.net
+t0.ssl.ak.tiles.virtualearth.net
+
+# Microsoft Backup bitlocker recovery key to Microsoft account
+g.live.com
+
+# Microsoft Edge Browser Update
+msedge.api.cdp.microsoft.com
+
+# Microsoft Office
+officeclient.microsoft.com
+
+# Microsoft Store (Windows Store)
+dl.delivery.mp.microsoft.com
+geo-prod.do.dsp.mp.microsoft.com
+displaycatalog.mp.microsoft.com
+
+# Microsoft Web Pages (Outlook, Office365, Live, Microsoft.com...)
+outlook.office365.com
+products.office.com
+c.s-microsoft.com
+i.s-microsoft.com
+login.live.com
+login.microsoftonline.com
+
+# Microsoft Windows 10 Update
+sls.update.microsoft.com.akadns.net
+fe3.delivery.dsp.mp.microsoft.com.nsatc.net
+tlu.dl.delivery.mp.microsoft.com
+
+# Microsoft Windows uses this to verify connectivity to Internet
+www.msftncsi.com
+
+# Skype
+s.gateway.messenger.live.com
+client-s.gateway.messenger.live.com
+ui.skype.com pricelist.skype.com
+apps.skype.com m.hotmail.com
+
+# Xbox Full Functionality
+xbox.ipv6.microsoft.com
+device.auth.xboxlive.com
+www.msftncsi.com
+title.mgt.xboxlive.com
+xsts.auth.xboxlive.com
+title.auth.xboxlive.com
+ctldl.windowsupdate.com
+attestation.xboxlive.com
+xboxexperiencesprod.experimentation.xboxlive.com
+xflight.xboxlive.com
+cert.mgt.xboxlive.com
+xkms.xboxlive.com
+def-vef.xboxlive.com
+notify.xboxlive.com
+help.ui.xboxlive.com
+licensing.xboxlive.com
+eds.xboxlive.com
+www.xboxlive.com
+v10.vortex-win.data.microsoft.com
+settings-win.data.microsoft.com
+
+# Xbox EA Play
+activity.windows.com
+
+# Xbox Live
+clientconfig.passport.net
+
+# Xbox Live Achievements (confirmed by Microsoft)
+v10.events.data.microsoft.com
+v20.events.data.microsoft.com
+
+# Xbox Live Messaging
+client-s.gateway.messenger.live.com
+
+# Xbox Store App on Series X/S
+arc.msn.com
+
+##########################
+### Sonstiges            #
+##########################
+
+# Android TV
+redirector.gvt1.com
+
+# Bild.de
+www.asadcdn.com
+code.bildstatic.de
+de.ioam.de
+json.bild.de
+script.ioam.de
+tags.tiqcdn.com
+tagger.opecloud.com
+
+# Burger King
+appboy-images.com
+rest.iad-03.braze.com
+
+# Cannon Printers
+gdlp01.c-wss.com
+
+# Chevrolet
+chevrolet.com
+
+# Crypto Currency
 bitcoin.de
+
+# Direct TV
+directvnow.com
+directvapplications.hb.omtrdc.net
+s.zkcdn.net
+js.maxmind.com
+
+# Tracking Packages sent with DPD
+tracking.dpd.de
+
+# Dropbox
+dl.dropboxusercontent.com
+ns1.dropbox.com
+ns2.dropbox.com
+
+# Epic Games Store
+tracking.epicgames.com
+
+# Fox News
+widget-cdn.rpxnow.com
+
+# GoDaddy webmail buttons
+imagesak.secureserver.net
+
+# GTA Online
+prod.telemetry.ros.rockstargames.com
+
+# Home Depot Checkout
+nexus.ensighten.com
+
+# Lowe's Checkout
+assets.adobedtm.com
+
+# Marketwatch.com Images
+s.marketwatch.com
+
+# Moto phones OS updates
+appspot-preview.l.google.com
+
+# Mozilla Firefox Tracking Protection
+tracking-protection.cdn.mozilla.net
+
+# NVIDIA GeForce Experience
+gfwsl.geforce.com
+
+# Origin (Savegame-Sync)
+cloudsync-prod.s3.amazonaws.com
+
+# Placehold.it
+placehold.it
+placeholdit.imgix.net
+
+# Playstation 5 "Recently Played Games" and Trophies
+telemetry-console.api.playstation.com
+
+# Plex
+plex.tv
+tvdb2.plex.tv
+pubsub.plex.bz
+proxy.plex.bz
+proxy02.pop.ord.plex.bz
+cpms.spop10.ams.plex.bz
+meta-db-worker02.pop.ric.plex.bz
+meta.plex.bz
+tvthemes.plexapp.com.cdn.cloudflare.net
+tvthemes.plexapp.com
+106c06cd218b007d-b1e8a1331f68446599e96a4b46a050f5.ams.plex.services
+meta.plex.tv
+cpms35.spop10.ams.plex.bz
+proxy.plex.tv
+metrics.plex.tv
+pubsub.plex.tv
+status.plex.tv
+www.plex.tv
+node.plexapp.com
+nine.plugins.plexapp.com
+staging.plex.tv
+app.plex.tv
+o1.email.plex.tv
+o2.sg0.plex.tv
+dashboard.plex.tv
+
+# Domains used by Plex
+gravatar.com
+thetvdb.com
+themoviedb.com
+chtbl.com
+
+# Red Hat Online Learning (subscription required)
+79423.analytics.edgekey.net
+
+# Reddit
+styles.redditmedia.com
+www.redditstatic.com
+reddit.map.fastly.net
+www.redditmedia.com
+reddit-uploaded-media.s3-accelerate.amazonaws.com
+
+# Signal
+ud-chat.signal.org
+chat.signal.org
+storage.signal.org
+signal.org
+www.signal.org
+updates2.signal.org
+textsecure-service-whispersystems.org
+giphy-proxy-production.whispersystems.org
+cdn.signal.org
+whispersystems-textsecure-attachments.s3-accelerate.amazonaws.com
+d83eunklitikj.cloudfront.net
+souqcdn.com
+cms.souqcdn.com
+api.directory.signal.org
+contentproxy.signal.org
+turn1.whispersystems.org
+
+# Sonarr
+services.sonarr.tv
+skyhook.sonarr.tv
+download.sonarr.tv
+apt.sonarr.tv
+forums.sonarr.tv
+
+# Spotify
+spclient.wg.spotify.com
+apresolve.spotify.com
+api-tv.spotify.com
+
+# Twitter
+twitter.com
+upload.twitter.com
+api.twitter.com
+mobile.twitter.com
+
+# Videos not playing in times.com and nydailynews.com
+delivery.vidible.tv
+img.vidible.tv
+videos.vidible.tv
+edge.api.brightcove.com
+cdn.vidible.tv
+
+# Videos not playing on weather.com
+v.w-x.co
+
+# WatchESPN
+fpdownload.adobe.com
+entitlement.auth.adobe.com
+livepassdl.conviva.com
+
+# Whatsapp
+wa.me
+www.wa.me
