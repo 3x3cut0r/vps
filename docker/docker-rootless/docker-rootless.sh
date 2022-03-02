5a765dd87d793bca69a3c35bb8f52882f4f6f63c
3x3cut0r <executor55@gmx.de>
2022-03-02 20:09:38 +0100

    docker-rootless.sh v1.03: debian 11 support added

--- docker/docker-rootless/docker-rootless.sh
+++ docker/docker-rootless/docker-rootless.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Author:   3x3cut0r <executor55@gmx.de>
-# Version:  1.02
-# Date:     2021-03-01
+# Version:  1.03
+# Date:     2022-03-04
 #
 # Description:
-#  this script installs docker and docker-compose in rootless mode on debian 10
+#  this script installs docker and docker-compose in rootless mode on debian 10 / 11
 #
 
 DOCKER_USERNAME="docker"
 DOCKER_UID="1000"
+DEBIAN_VERSION=$(cat /etc/os-release | grep VERSION_ID | cut -d= -f2 | tr -d '"')
 
 # change username
 function username () {
@@ -18,14 +19,27 @@ function username () {
     echo ""
     read -p "Enter the username under which the docker service should run. ($DOCKER_USERNAME): "
     if [ ! -z $REPLY ]; then
-        if [ $(id -u $REPLY 2> /dev/null ) ]; then
-            DOCKER_USERNAME=$REPLY
-            DOCKER_UID="$(id -u $DOCKER_USERNAME)"
+        DOCKER_USERNAME=$REPLY
+    fi
+
+    if [ $(id -u $DOCKER_USERNAME 2> /dev/null ) ]; then
+        DOCKER_UID="$(id -u $DOCKER_USERNAME)"
+    else
+        printf '\e[1;31m%-6s\e[m\n'  "username: $DOCKER_USERNAME does not exist."
+        read -p "Do you want to create it? (n/Y): "
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            read -p "Enter new UID/GID ($DOCKER_UID): "
+            if [ ! -z "$REPLY" ]; then
+                DOCKER_UID=$REPLY
+            fi
+            addgroup --gid $DOCKER_UID $DOCKER_USERNAME
+            adduser --uid $DOCKER_UID --gid $DOCKER_UID --shell /bin/bash --disabled-password --gecos '' $DOCKER_USERNAME
         else
-            printf '\e[1;31m%-6s\e[m\n'  "username: $REPLY does not exits "
-            echo "using $DOCKER_USERNAME instead"
+            printf '\e[1;31m%-6s\e[m\n'  "you need a docker user ... aborting"
+            exit 1
         fi
     fi
+    echo ""
     echo "DOCKER_USERNAME: $DOCKER_USERNAME (uid=$DOCKER_UID)"
 }
 
@@ -52,13 +66,25 @@ function prepare () {
     printf '\n\e[0;33m%-6s\e[m\n' " ==> apt: install prerequisites ... "
     apt update && apt upgrade -y
     apt install \
+            apt-transport-https \
+            ca-certificates \
             curl \
+            gnupg-agent \
+            iptables \
             openssh-server \
+            software-properties-common \
             sudo \
             uidmap \
             unzip \
             wget \
             -y
+    if [[ $DEBIAN_VERSION -eq "11" ]]; then
+        apt install \
+                dbus-user-session \
+                fuse-overlayfs \
+                slirp4netns \
+                -y
+    fi
 
     # vim: arrow key fix
     printf '\n\e[0;33m%-6s\e[m\n' " ==> vim: arrow key fix ... "
@@ -80,11 +106,19 @@ function prepare () {
         echo -e "\n# enable vm.overcommit_memory (e.g. for redis)" >> /etc/sysctl.conf
         echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
     fi
-    
+
+    # enable-linger
+    printf '\n\e[0;33m%-6s\e[m\n' " ==> docker: loginctl enable-linger $DOCKER_USERNAME "
+    loginctl enable-linger $DOCKER_USERNAME
+
     # reboot
-    printf '\n\e[0;33m%-6s\e[m\n' " ==> reboot, then login (via ssh) as $DOCKER_USERNAME (uid=$DOCKER_UID) to continue with './docker_rootless.sh --install'"
+    printf '\n\e[0;33m%-6s\e[m\n' " ==> reboot, login with root, passwd $DOCKER_USERNAME, logout, then login (via ssh) as $DOCKER_USERNAME (uid=$DOCKER_UID) to continue with './docker_rootless.sh --install'"
     read -n 1 -s -r -p "press any key to continue ..."
-    /usr/sbin/reboot
+    if [[ $DEBIAN_VERSION -eq "11" ]]; then
+        /usr/sbin/reboot
+    else
+        /sbin/reboot
+    fi
 }
 
 # second run as docker
@@ -110,20 +144,19 @@ function install () {
     # install rootless docker
     if [ $UID -eq 0 ]; then echo "You need to login (via ssh) as $DOCKER_USERNAME (uid=$DOCKER_UID) to install rootless docker!"; exit 1; fi
     curl -fsSL https://get.docker.com/rootless | sh
-    sudo loginctl enable-linger docker
 
-    # set docker-compose version
+    # set docker-compose v1 version
     DOCKER_COMPOSE_VERSION=$(curl -L "https://docs.docker.com/compose/install/" | grep -o -P '(?<=https://github.com/docker/compose/releases/download/).*(?=/docker-compose)' | head -n1)
-    if [ $DOCKER_COMPOSE_VERSION = "" ]; then DOCKER_COMPOSE_VERSION="1.28.4"; fi
-    printf '\n\e[0;33m%-6s\e[m\n' " ==> docker-compose: install rootless docker-compose ... "
-    read -p "Which docker-compose version do you want to install? ($DOCKER_COMPOSE_VERSION): "
+    if [ $DOCKER_COMPOSE_VERSION = "" ]; then DOCKER_COMPOSE_VERSION="1.29.2"; fi
+    printf '\n\e[0;33m%-6s\e[m\n' " ==> docker-compose v1: install docker-compose v$DOCKER_COMPOSE_VERSION ... "
+    read -p "Which docker-compose v1 version do you want to install? ($DOCKER_COMPOSE_VERSION): "
     if [ ! -z $REPLY ]; then
         if [ $(id -u $REPLY 2> /dev/null ) ]; then
             DOCKER_COMPOSE_VERSION=$REPLY
         fi
     fi
 
-    # install rootless docker-compose
+    # install rootless docker-compose v1
     sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
@@ -131,15 +164,37 @@ function install () {
     # install docker-compose bash completion
     sudo curl -L "https://raw.githubusercontent.com/docker/compose/$DOCKER_COMPOSE_VERSION/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
 
+    # install rootless docker-compose v2
+    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+    DOCKER_COMPOSE_VERSION_v2=$(curl -L https://docs.docker.com/compose/cli-command/#install-on-linux | grep '<span class="go"> Docker Compose version' | cut -d' ' -f6)
+    if [ $DOCKER_COMPOSE_VERSION_v2 = "" ]; then DOCKER_COMPOSE_VERSION_v2="2.2.3"; fi
+    printf '\n\e[0;33m%-6s\e[m\n' " ==> docker-compose v2: install docker-compose v$DOCKER_COMPOSE_VERSION_v2 ... "
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    sudo curl -SL https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION_v2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+    sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+    docker compose version
+
+    # install compose switch
+    DOCKER_COMPOSE_SWITCH_VERSION=$(curl -L https://github.com/docker/compose-switch/releases | grep /docker/compose-switch/releases/download/ | head -n1 | grep -o -P '(?<=/docker/compose-switch/releases/download/).*(?=/docker-compose)')
+    if [ $DOCKER_COMPOSE_SWITCH_VERSION = "" ]; then DOCKER_COMPOSE_SWITCH_VERSION="v1.0.4"; fi
+    printf '\n\e[0;33m%-6s\e[m\n' " ==> docker compose switch: install docker compose switch $DOCKER_COMPOSE_SWITCH_VERSION ... "
+    sudo curl -fL https://github.com/docker/compose-switch/releases/download/$DOCKER_COMPOSE_SWITCH_VERSION/docker-compose-linux-amd64 -o /usr/local/bin/compose-switch
+    sudo chmod +x /usr/local/bin/compose-switch
+    sudo mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose-v1
+    sudo update-alternatives --install /usr/local/bin/docker-compose docker-compose /usr/local/bin/docker-compose-v1 1
+    sudo update-alternatives --install /usr/local/bin/docker-compose docker-compose /usr/local/bin/compose-switch 99
+    sudo update-alternatives --display docker-compose
+
     # prepare ~/.bashrc
     echo -e "\n# Docker environment variables" >> ~/.bashrc
+    echo "export XDG_RUNTIME_DIR=/home/$DOCKER_USERNAME/.docker/run" >> ~/.bashrc
     echo "if [[ ! \$(echo \$PATH | grep /home/$DOCKER_USERNAME/bin) ]]; then" >> ~/.bashrc
     echo "    export PATH=/home/$DOCKER_USERNAME/bin:\$PATH" >> ~/.bashrc
     echo "fi" >> ~/.bashrc
     echo "export DOCKER_HOST=unix:///run/user/$DOCKER_UID/docker.sock" >> ~/.bashrc
 
     # prepare /etc/sudoers
-    if [[ ! $(sudo cat /etc/sudoers | grep secure_path | grep /home/docker/bin) ]]; then
+    if [[ ! $(sudo cat /etc/sudoers | grep secure_path | grep /home/$DOCKER_USERNAME/bin) ]]; then
         sudo sed -i "s#secure_path=\"#secure_path=\"/home/$DOCKER_USERNAME/bin:#g" /etc/sudoers
     fi
     if [[ ! $(sudo cat /etc/sudoers | grep env_keep | grep DOCKER_HOST) ]]; then
@@ -159,7 +214,11 @@ function install () {
     printf '\n\e[0;33m%-6s\e[m\n' " ==> reboot ... login with docker ... and use 'docker ...'"
     printf '\e[0;33m%-6s\e[m\n\n' " ==> OPTIONAL: remove $DOCKER_USERNAME from sudo group (sudo deluser $DOCKER_USERNAME sudo)"
     read -n 1 -s -r -p "press any key to reboot ..."
-    sudo /usr/sbin/reboot
+    if [[ $DEBIAN_VERSION -eq "11" ]]; then
+        sudo /usr/sbin/reboot
+    else
+        sudo /sbin/reboot
+    fi
 }
 
 function help () {
