999f99f8e9bf79be487ab73c3d158ef1fb7e9cc5
3x3cut0r <executor55@gmx.de>
2023-10-07 16:20:59 +0300

    Update tools.sh

--- basics/tools.sh
+++ basics/tools.sh
@@ -3,14 +3,17 @@ apt install     apache2-utils \
                 bash-completion \
                 curl \
                 dnsutils \
+                gcc \
                 htop \
                 iftop \
                 linux-headers-$(uname -r) \
+                make \
                 nano \
                 net-tools \
                 ncdu \
                 nmon \
                 openssh-server \
+                perl \
                 pwgen \
                 ripgrep \
                 rsync \
