b3f9f8b45e95b96d4aebc30fd6ed1b77f08733b9
3x3cut0r <executor55@gmx.de>
2025-11-25 19:09:18 +0100

    Update removeSubscriptionNotice.sh

--- opt/scripts/removeSubscriptionNotice.sh
+++ opt/scripts/removeSubscriptionNotice.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Remove proxmox subscription notice after login
 cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.bak
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
