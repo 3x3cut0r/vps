56f0d69f186ae015ecb09b4bb5cbec1dada0fee4
3x3cut0r <executor55@gmx.de>
2025-11-25 19:54:09 +0100

    Update mirror_ports.sh

--- opt/scripts/network/mirror_ports.sh
+++ opt/scripts/network/mirror_ports.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# Usage:
+#   ./mirror_ports.sh <INPUT> <OUTPUT>
+#
+# Example:
+#   ./mirror_ports.sh vmbr0 vmbr1
+#
 
-# set parameters
+# Set parameters
 INPUT="${1:-vmbr0}"
 OUTPUT="${2:-vmbr1}"
 
-# port mirror
+# Port mirror
 tc qdisc add dev $INPUT ingress
 tc filter add dev $INPUT parent ffff: protocol all u32 match u8 0 0 action mirred egress mirror dev $OUTPUT
